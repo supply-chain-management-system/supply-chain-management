7fa36e105e5fd4aa7cfa0c5b7ce94c6d561b5481
shadirvan <shadirvant111@gmail.com>
2026-04-29 12:49:09 +0530

    SP-11:Edited the model add company model also

--- server/app/db/database.py
+++ server/app/db/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:12345@localhost:5432/supply_chain_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
