ab76858478fe79ca9b79c1be7141cadc9d1d1a96
shadirvan <141410453+Shadirvan1@users.noreply.github.com>
2026-04-29 17:38:03 +0530

    Delete client/.vite/deps directory

--- client/.vite/deps/react.js
+++
@@ -1,6 +0,0 @@
-<<<<<<< HEAD
-import { t as require_react } from "./react-DTjSBzeh.js";
-=======
-import { t as require_react } from "./react-DW0Z4HR0.js";
->>>>>>> development
-export default require_react();
